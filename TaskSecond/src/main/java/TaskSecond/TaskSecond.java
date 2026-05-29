634fc0fd336e9e65e8de65b23a5cb32e901cb19e
basko84 <basko_olga@mail.ru>
2026-05-29 09:35:26 +0300

    commit TaskSecond

---
+++ TaskSecond/src/main/java/TaskSecond/TaskSecond.java
@@ -0,0 +1,26 @@
+package TaskSecond;
+
+import java.util.Scanner;
+
+public class TaskSecond {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первую строку (a): ");
+        String a = scanner.nextLine();
+
+        System.out.print("Введите вторую строку (b): ");
+        String b = scanner.nextLine();
+
+        // Сравнение строк
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+
+        scanner.close();
+    }
+}
+
+
