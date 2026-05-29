b324d0cef76eced00abac71a638421e3840f72fd
basko84 <basko_olga@mail.ru>
2026-05-29 09:43:07 +0300

    commit TaskThree

---
+++ TaskThree/src/main/java/TaskThree/TaskThree.java
@@ -0,0 +1,17 @@
+package TaskThree;
+
+public class TaskThree {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Чётные числа данного массива:");
+
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+    }
+}
+
+
